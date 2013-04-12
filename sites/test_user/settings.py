9489ff0934c17045e2d8ec0230a05c23116c8b57
Corbin Child <corbinchild@avant-garde.co.nz>
2013-04-12 22:58:10 +1200

    Multiple sites.

---
+++ sites/test_user/settings.py
@@ -0,0 +1,16 @@
+import os
+from django.conf import settings
+
+SITE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+MEDIA_ROOT = os.path.join(SITE_PATH, "media")
+
+MEDIA_URL = os.path.relpath(MEDIA_ROOT, settings.PROJECT_ROOT)
+MEDIA_URL = os.sep + os.path.normpath(MEDIA_URL) + os.sep
+
+# Set this so that when we run collectstatic,
+# user edited files will get pulled in.
+STATICFILES_DIRS = (
+	os.path.join(settings.PROJECT_ROOT, "static"),
+    os.path.join(SITE_PATH, "static")
+)
